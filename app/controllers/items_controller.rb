9dbf292b918661f440a08946cf9ee3b7c429e2bf
kinugasa00 <yushi.nishi@gmail.com>
2021-05-14 04:56:09 +0900

    Basic認証を導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show, :require_login]
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
+    
   end
-  
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+
 end
