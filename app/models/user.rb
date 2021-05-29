7120bc409988b803abcf14191c486892cc0c98d5
kinugasa00 <yushi.nishi@gmail.com>
2021-05-29 20:01:56 +0900

    ユーザー管理機能更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true,
+  with_options presence: true do
+    validates :nickname
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-    validates :last_name
-    validates :first_name
-  end
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :last_name
+      validates :first_name
+   end
   
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
 
-  validates :birthday,        presence: true
+    validates :birthday
+  end
 end
