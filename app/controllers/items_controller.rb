592849260c0331a9deb2ab3b3d11344aeec9692f
kinugasa00 <yushi.nishi@gmail.com>
2021-06-08 11:07:59 +0900

    コメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,26 +23,26 @@ class ItemsController < ApplicationController
       end
   end
 
-  def edit
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to root_path
-    end
-  end
+  # def edit
+  #   unless user_signed_in? && current_user.id == @item.user_id
+  #     redirect_to root_path
+  #   end
+  # end
 
-  def update
-    if @item.update(item_params)
-      redirect_to item_path(@item.id)
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   if @item.update(item_params)
+  #     redirect_to item_path(@item.id)
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def destroy
-    if user_signed_in? && current_user.id == @item.user_id
-      @item.destroy
-      redirect_to root_path
-    end
-  end
+  # def destroy
+  #   if user_signed_in? && current_user.id == @item.user_id
+  #     @item.destroy
+  #     redirect_to root_path
+  #   end
+  # end
 
   private
 
