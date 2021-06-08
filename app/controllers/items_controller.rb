4193eb111991d0220bd8f78ca53dced23410008c
kinugasa00 <yushi.nishi@gmail.com>
2021-06-08 17:12:07 +0900

    controller訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :redirect, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -45,7 +46,7 @@ class ItemsController < ApplicationController
   private
 
   def redirect
-    redirect_to action: :index unless user_signed_in?
+    redirect_to action: :index unless user_signed_in? && current_user.id
   end
 
   def set_item
