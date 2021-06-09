fc0018bcc895edac9586362a45e6b8a69e0bc0e9
kinugasa00 <yushi.nishi@gmail.com>
2021-06-10 00:24:50 +0900

    テストコード修正

--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ItemOrder, type: :model do
-  describe '購入者情報の保存' do
-    before do
-      @item_order = FactoryBot.build(:item_order)
-    end
+  before do
+    @item_order = FactoryBot.build(:item_order)
+  end
 
+  describe '購入者情報の保存' do
+    context '購入が保存できる場合' do
       it '全ての項目が入力されていれば購入ができる' do
         expect(@item_order).to be_valid
       end
+    end
+
+    context 'うまくいかないとき' do
       it 'token(クレジットカード情報)が空だと購入ができない' do
         @item_order.token = nil
         @item_order.valid?
@@ -17,7 +21,7 @@ RSpec.describe ItemOrder, type: :model do
       it '郵便番号が空だと購入ができない' do
         @item_order.postal_code = ""
         @item_order.valid?
-        expect(@item_order.errors.messages).to include({:postal_code => ["can't be blank", "is invalid", "is the wrong length (should be 8 characters)"]})
+        expect(@item_order.errors.messages).to include({:postal_code => ["can't be blank", "is invalid", "can't be blank", "is invalid", "is the wrong length (should be 8 characters)"]})
       end
       it '郵便番号にハイフンがないと登録できない' do
         @item_order.postal_code = "12345678"
@@ -27,7 +31,7 @@ RSpec.describe ItemOrder, type: :model do
       it '郵便番号が8桁でないと購入できない' do
         @item_order.postal_code = "123-458"
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Postal code is invalid", "Postal code is the wrong length (should be 8 characters)")
+        expect(@item_order.errors.full_messages).to include("Postal code is the wrong length (should be 8 characters)")
       end
       it 'prefecture_idが空だと購入できない' do
         @item_order.prefecture_id = nil
@@ -47,12 +51,13 @@ RSpec.describe ItemOrder, type: :model do
       it 'phone_numberが空だと購入できない' do
         @item_order.phone_number = ""
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Phone number can't be blank", "Phone number is invalid")
+        expect(@item_order.errors.full_messages).to include("Phone number can't be blank")
       end
       it 'phone_numberが11桁でなければ購入できない' do
         @item_order.phone_number = "080123456789"
         @item_order.valid?
         expect(@item_order.errors.full_messages).to include("Phone number is invalid")
       end
+    end
   end
 end
